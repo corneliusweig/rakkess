9ee9c852fa1fc6347cdac264a68090c2f83af947
Cornelius Weig <cornelius.weig@tngtech.com>
2019-04-20 21:24:51 +0200

    Add new subcommand "resource"

---
+++ cmd/resource.go
@@ -0,0 +1,41 @@
+/*
+Copyright 2019 Cornelius Weig
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// resourceCmd represents the resource command
+var resourceCmd = &cobra.Command{
+	Use:     "resource",
+	Aliases: []string{"for", "for-resource"},
+	Short:   "Show access matrix for a given resource",
+	Args:    cobra.ExactArgs(1),
+	Long:    `todo`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("resource called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(resourceCmd)
+
+	AddRakkessFlags(resourceCmd)
+}
--- cmd/root.go
+++ cmd/root.go
@@ -68,6 +68,7 @@ var rootCmd = &cobra.Command{
 	Short:   "Review access - show an access matrix for all resources",
 	Long:    rakkessLongDescription,
 	Example: rakkessExamples,
+	Args:    cobra.NoArgs,
 	Run: func(cmd *cobra.Command, args []string) {
 		ctx, cancel := context.WithCancel(context.Background())
 		catchCtrC(cancel)
@@ -90,10 +91,7 @@ func Execute() {
 func init() {
 	rootCmd.PersistentFlags().StringVarP(&v, "verbosity", "v", constants.DefaultLogLevel.String(), "Log level (debug, info, warn, error, fatal, panic)")
 
-	rootCmd.Flags().StringSliceVar(&rakkessOptions.Verbs, "verbs", []string{"list", "create", "update", "delete"}, fmt.Sprintf("show access for verbs out of %s", constants.ValidVerbs))
-	rootCmd.Flags().StringVarP(&rakkessOptions.Output, "output", "o", "icon-table", fmt.Sprintf("output format out of %s", constants.ValidOutputFormats))
-
-	rakkessOptions.ConfigFlags.AddFlags(rootCmd.Flags())
+	AddRakkessFlags(rootCmd)
 
 	rootCmd.PersistentPreRunE = func(cmd *cobra.Command, args []string) error {
 		if err := SetUpLogs(rakkessOptions.Streams.ErrOut, v); err != nil {
@@ -101,6 +99,13 @@ func init() {
 		}
 		return nil
 	}
+}
+
+func AddRakkessFlags(cmd *cobra.Command) {
+	cmd.Flags().StringSliceVar(&rakkessOptions.Verbs, "verbs", []string{"list", "create", "update", "delete"}, fmt.Sprintf("show access for verbs out of %s", constants.ValidVerbs))
+	cmd.Flags().StringVarP(&rakkessOptions.Output, "output", "o", "icon-table", fmt.Sprintf("output format out of %s", constants.ValidOutputFormats))
+
+	rakkessOptions.ConfigFlags.AddFlags(cmd.Flags())
 }
 
 func SetUpLogs(out io.Writer, level string) error {
