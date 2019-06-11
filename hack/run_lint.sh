1df34c49b9c424be4ad6e8dc788ad9c5b42d1987
Cornelius Weig <cornelius.weig@tngtech.com>
2019-06-12 00:33:22 +0200

    Upgrade golangci-lint to v1.17.1

--- hack/run_lint.sh
+++ hack/run_lint.sh
@@ -21,7 +21,7 @@ HACK=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 if ! [[ -x "$GOPATH/bin/golangci-lint" ]]
 then
    echo 'Installing golangci-lint'
-   "${HACK}"/install_golangci-lint.sh -b "$GOPATH/bin" v1.16.0
+   "${HACK}"/install_golangci-lint.sh -b "$GOPATH/bin" v1.17.1
 fi
 
 "$GOPATH/bin/golangci-lint" run \
